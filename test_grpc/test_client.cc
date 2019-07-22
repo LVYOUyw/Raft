5dedd331918c4349d9a36fa321ef056647cb4758
LVYOUyw <18617850785@163.com>
2019-07-22 17:06:29 +0800

    update connect

--- test_grpc/test_client.cc
+++ test_grpc/test_client.cc
@@ -20,7 +20,7 @@ class Client
     request.set_request(user);
     Vresponse reply;
     ClientContext context;
-    Status status = stub_->testhello(&context, request, &reply);
+    Status status = stub_->modify(&context, request, &reply);
     if (status.ok()) {
       return reply.response();
     } else {
@@ -36,8 +36,8 @@ class Client
 
 int main(int argc, char** argv) {
   Client greeter(grpc::CreateChannel(
-      "0.0.0.0:50051", grpc::InsecureChannelCredentials()));
-  std::string user("world");
+      "0.0.0.0:50052", grpc::InsecureChannelCredentials()));
+  std::string user("Vergil");
   std::string reply = greeter.Test(user);
   std::cout << "Greeter received: " << reply << std::endl;
 
--- test_grpc/test_server.cc
+++ test_grpc/test_server.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <grpcpp/grpcpp.h>
+#include <thread>
 #include "test_proto.grpc.pb.h"
 
 using grpc::Server;
@@ -11,20 +12,65 @@ using grpc::Status;
 using test::Vrequest;
 using test::Vresponse;
 using test::Vergil;
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
 
 class ServiceImpl final : public Vergil::Service 
 {
-  Status testhello(ServerContext* context, const Vrequest* request, Vresponse* reply) override 
-  {
-    std::string prefix("Hello ");
-    reply->set_response(prefix + request->request());
-    return Status::OK;
-  }
+  public:
+      ServiceImpl(std::uint16_t port) : port(port) 
+      {
+        key_name=std::to_string(port);
+        std::uint16_t p = 50051;
+        for (int i = 1; i <=3 ; i++) 
+        {
+            if (p == port) {p++;continue;}
+            std::shared_ptr<Channel> channel = grpc::CreateChannel("0.0.0.0:" + std::to_string(p),
+                                               grpc::InsecureChannelCredentials());
+            std::unique_ptr<Vergil::Stub> tmp = Vergil::NewStub(channel);
+            st.emplace_back(std::move(tmp));
+            p++;
+        }
+      } 
+        
+      Status modify(ServerContext* context, const Vrequest* request, Vresponse* reply) override 
+      {
+        key_name = request -> request();
+        std::string answer = std::to_string(port) + ": modify OK";
+        reply->set_response(answer);
+        std::uint16_t p = 50051;
+        for (auto & stub_ : st) 
+        {
+            Vrequest req;
+            req.set_request(request -> request());
+            Vresponse rep;
+            ClientContext cont;
+            Status status = stub_->smodify(&cont, req, &rep);
+            if (!status.ok()) 
+                std::cout << status.error_code() << ": " << status.error_message() << "\n";
+            std::cout << rep.response() << "\n";
+        }
+        return Status::OK;
+      }
+      
+      Status smodify(ServerContext* context, const Vrequest* request, Vresponse* reply) override 
+      {
+        key_name = request -> request();
+        std::string answer = std::to_string(port) + ": modify OK";  
+        reply -> set_response(answer);
+        return Status::OK;
+      }
+  
+  private:
+       std::string key_name;
+       std::uint16_t port;
+       std::vector<std::unique_ptr<Vergil::Stub>> st;
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  ServiceImpl service;
+void RunServer(std::uint16_t port) {
+  std::string server_address("0.0.0.0:" + std::to_string(port));
+  ServiceImpl service(port);
   ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
@@ -35,6 +81,11 @@ void RunServer() {
 
 int main(int argc, char** argv) 
 {
-  RunServer();
+  std::thread t1(RunServer,50051);
+  std::thread t2(RunServer,50052);
+  std::thread t3(RunServer,50053);
+  t1.join();
+  t2.join();
+  t3.join();
   return 0;
 }
